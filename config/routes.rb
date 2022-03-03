f7e9037d3856d772729d0f1ec71e214a74a951f4
txphuc <txp.sun@gmail.com>
2022-03-03 09:43:37 -0800

    Sign Up

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,15 @@ Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
     root "static_pages#home"
 
+    # Static Pages
     get "/home", to: "static_pages#home"
     get "/help", to: "static_pages#help"
     get "/about", to: "static_pages#about"
     get "/contact", to: "static_pages#contact"
 
+    #Users
+    get "/signup", to: "users#new"
+    post "/signup", to: "users#create"
+    resources :users
   end
 end
